f4981f513cb536b779b7f235edcb9655b7ef812d
Vunas <vuh265@gmail.com>
2024-09-29 21:50:28 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,21 @@
 window.addEventListener('scroll', function() {
-    const content = document.querySelector('.head__nav');
+    const nav = document.querySelector('.navbar');
+    var brand = document.getElementById('brand');
+    var brand_item = document.getElementById('brand-item');
+
     if (window.scrollY > 50) {
-        content.classList.add('head__nav-new');
+        nav.classList.add('add');
+        brand.className= 'small__brand';
+        brand_item.className= 'small__brand-item';
     } else {
-        content.classList.remove('head__nav-new');
+        nav.classList.remove('add');
+        brand.className= 'brand';
+        brand_item.className= 'brand-item';
     }
 });
 
-window.addEventListener('scroll', function() {
-    var element = document.getElementById('myElement');
-    if (window.scrollY > 50) { // Kiểm tra nếu đã cuộn hơn 50px
-        element.className = 'head__nav-li head__nav-item head__nav-item-link small-brand';
-    } else {
-        element.className = 'head__nav-li head__nav-item head__nav-item-link brand';
-    }
-});
+
+   
+
+
 
