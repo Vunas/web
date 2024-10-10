f22daa43e1c38955d81b6b5808b6b43ebca3bcc5
Vunas <vuh265@gmail.com>
2024-10-10 18:29:26 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,26 @@
-window.addEventListener('scroll', function() {
-    const nav = document.querySelector('.navbar');
-    var brand = document.getElementById('brand');
-    var brand_item = document.getElementById('brand-item');
+window.addEventListener("scroll", function () {
+  const nav = document.querySelector(".navbar");
+  var brand = document.getElementById("brand");
+  var brand_item = document.getElementById("brand-item");
 
-    if (window.scrollY > 50) {
-        nav.classList.add('add');
-        brand.className= 'small__brand';
-        brand_item.className= 'small__brand-item';
-    } else {
-        nav.classList.remove('add');
-        brand.className= 'brand';
-        brand_item.className= 'brand-item';
-    }
+  if (window.scrollY > 30) {
+    nav.classList.add("add");
+    brand.className = "small__brand";
+    brand_item.className = "small__brand-item";
+  } else {
+    nav.classList.remove("add");
+    brand.className = "brand";
+    brand_item.className = "brand-item";
+  }
 });
 
+function search() {
+  document.getElementById("modal").style.display = "block";
+  document.getElementById("search_div").style.display = "block";
 
-   
+}
+function closes() {
+  document.getElementById("modal").style.display = "none";
+  document.getElementById("search_div").style.display = "none";
 
-
-
+}
